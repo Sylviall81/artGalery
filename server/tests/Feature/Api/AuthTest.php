d2e426df5e174a92f52edbb7a03f2c2ffa72ab45
Sylvia Llorente <sylviall81@gmail.com>
2023-09-18 19:18:26 +0200

    importar refreshdatabase y hash en register test

--- server/tests/Feature/Api/AuthTest.php
+++ server/tests/Feature/Api/AuthTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -11,10 +13,18 @@ class AuthTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+     use RefreshDatabase;
+
+    public function test_user_can_register(): void
+    {
+        $this->postJson('api/register', [
+            'name' => 'Elena',
+            'email'=>'e.mail@gmail.com',
+            'password' => Hash::make ('Ee123456789*')
+        ]);
+
+        $this->assertCount(1, User::all());
+       
     }
 }
